95241fd4f001c12ab92f7360de28793cae57a364
sojdh <ekdrhr990715@gmail.com>
2023-03-23 23:16:42 +0900

    다시

---
+++ RecipeNinja/src/main/java/com/ysu/recipeninja/RecipeNinjaApplication.java
@@ -0,0 +1,13 @@
+package com.ysu.recipeninja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeNinjaApplication.class, args);
+		
+	}
+}
