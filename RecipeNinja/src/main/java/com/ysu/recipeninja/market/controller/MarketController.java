c4f7feca4cac15f26f531610aa1cfd3ae2b85ea2
rjatk <rjatk@DESKTOP-1K0FRDK>
2023-04-13 19:28:01 +0900

    ....

--- RecipeNinja/src/main/java/com/ysu/recipeninja/market/controller/MarketController.java
+++ RecipeNinja/src/main/java/com/ysu/recipeninja/market/controller/MarketController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.ModelAndView;
 
 	@Controller
@@ -12,7 +13,15 @@ import org.springframework.web.servlet.ModelAndView;
 		@RequestMapping("Market")
 		public ModelAndView market() {
 			return new ModelAndView("market/MarketMain");
+	
+	
 		}
-
+	@Controller
+	public class ProductSubPage{
+		@RequestMapping("ProductSubPage")
+		public ModelAndView productsubpage() {
+			return new ModelAndView("market/ProductSubPage");
+		}
+	}
 	
 }
